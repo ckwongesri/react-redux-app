31cbba163d4ab52908a9763a51edffe00fcd5dda
ckwong <ckwong@esrisingapore.com.sg>
2022-06-14 14:56:54 +0800

    Implemented UsersList and UserPage

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import {
 import { AddPostForm } from "@features/posts/AddPostForm";
 import { PostsList } from "@features/posts/PostsList";
 import { Navbar } from "./app/Navbar";
-import { SinglePostPage } from "./features/posts/SinglePostPage";
-import { EditPostForm } from "./features/posts/EditPostForm";
+import { SinglePostPage } from "@features/posts/SinglePostPage";
+import { EditPostForm } from "@features/posts/EditPostForm";
+import { UsersList } from "@features/users/UsersList";
+import { UserPage } from "@features/users/UserPage";
 
 function App() {
   return (
@@ -29,6 +31,9 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+
           <Redirect to="/" />
         </Switch>
       </div>
