4c5b61ed155965441c9aee15a408ad23ed3e602f
ckwong <ckwong@esrisingapore.com.sg>
2022-06-14 16:10:13 +0800

    Implemented manual refresh

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,6 +5,7 @@ import { Spinner } from "../../components/Spinner";
 import { PostAuthor } from "../users/PostAuthor";
 import { ReactionButtons } from "./ReactionButtons";
 import { TimeAgo } from "./TimeAgo";
+import classnames from "classnames";
 
 let PostExcerpt = ({ post }) => {
   return (
@@ -29,9 +30,11 @@ function PostsList() {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
+    refetch,
   } = useGetPostsQuery();
 
   const sortedPosts = useMemo(() => {
@@ -45,9 +48,13 @@ function PostsList() {
   if (isLoading) {
     content = <Spinner text="Loading..." />;
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ));
+    const containerClassname = classnames("posts-container", {
+      disabled: isFetching,
+    });
+    content = <div className={containerClassname}>{renderedPosts}</div>;
   } else if (isError) {
     content = <p>{error.toString()}</p>;
   }
@@ -55,6 +62,7 @@ function PostsList() {
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   );
