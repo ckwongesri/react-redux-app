40f64cc8e5b4a7a2d0680f6dee02163a2d1f14a9
ckwong <ckwong@esrisingapore.com.sg>
2022-06-14 14:36:48 +0800

    Implemented async post listing

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { PostAuthor } from "../users/PostAuthor";
-import { selectAllPosts } from "./postsSlice";
+import { fetchPosts, selectAllPosts } from "./postsSlice";
 import { ReactionButtons } from "./ReactionButtons";
 import { TimeAgo } from "./TimeAgo";
+import { Spinner } from "../../components/Spinner";
 
-function PostsList() {
-  const posts = useSelector(selectAllPosts);
-
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-
-  const renderedPosts = orderedPosts.map((post) => (
+function PostExcerpt({ post }) {
+  return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <div>
@@ -26,12 +21,42 @@ function PostsList() {
         Read More
       </Link>
     </article>
-  ));
+  );
+}
+
+function PostsList() {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+
+  const postStatus = useSelector((state) => state.posts.status);
+  const error = useSelector((state) => state.posts.error);
+
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  let content;
+
+  if (postStatus === "loading") {
+    content = <Spinner text="Loading..." />;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,16 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-import { sub } from "date-fns";
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
+import { client } from "../../api/client";
 
 const initialState = {
   posts: [],
   status: "idle",
   error: null,
 };
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await client.get("/fakeApi/posts");
+  return response.data;
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -43,6 +48,20 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++;
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.posts = state.posts.concat(action.payload);
+        state.status = "succeeded";
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.status = "failed";
+      });
   },
 });
 
