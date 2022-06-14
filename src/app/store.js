c3120b10f47fe1882877a956000597b5bf576658
ckwong <ckwong@esrisingapore.com.sg>
2022-06-14 15:59:20 +0800

    Implemented RTK Query for getting all posts

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "@features/api/apiSlice";
+import notificationsReducer from "@features/notifications/notificationsSlice";
 import postsReducer from "@features/posts/postsSlice";
 import usersReducer from "@features/users/usersSlice";
-import notificationsReducer from "@features/notifications/notificationsSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useGetPostsQuery } from "@features/api/apiSlice";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { Spinner } from "../../components/Spinner";
 import { PostAuthor } from "../users/PostAuthor";
-import { fetchPosts, selectPostById, selectPostIds } from "./postsSlice";
 import { ReactionButtons } from "./ReactionButtons";
 import { TimeAgo } from "./TimeAgo";
 
-let PostExcerpt = ({ postId }) => {
-  const post = useSelector((state) => selectPostById(state, postId));
+let PostExcerpt = ({ post }) => {
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -28,28 +26,30 @@ let PostExcerpt = ({ postId }) => {
 PostExcerpt = React.memo(PostExcerpt);
 
 function PostsList() {
-  const dispatch = useDispatch();
-  const orderPostsIds = useSelector(selectPostIds);
+  const {
+    data: posts = [],
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsQuery();
 
-  const postStatus = useSelector((state) => state.posts.status);
-  const error = useSelector((state) => state.posts.error);
-
-  useEffect(() => {
-    if (postStatus === "idle") {
-      dispatch(fetchPosts());
-    }
-  }, [postStatus, dispatch]);
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = posts.slice();
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date));
+    return sortedPosts;
+  }, [posts]);
 
   let content;
 
-  if (postStatus === "loading") {
+  if (isLoading) {
     content = <Spinner text="Loading..." />;
-  } else if (postStatus === "succeeded") {
-    content = orderPostsIds.map((postId) => (
-      <PostExcerpt key={postId} postId={postId} />
+  } else if (isSuccess) {
+    content = sortedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
     ));
-  } else if (postStatus === "failed") {
-    content = <p>{error}</p>;
+  } else if (isError) {
+    content = <p>{error.toString()}</p>;
   }
 
   return (
